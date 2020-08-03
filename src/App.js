f53b587882f30127f0f9947cf4c638bed54c587d
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-08-03 14:55:07 -0400

    set up hardcoded table, with search bar. Added hardcoded employees array

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component  {
+  state = {
+    sortAscending: true,
+    employees: [
+      {
+      id: "1",
+      employee_name: "Tiger Nixon",
+      employee_salary: "320800",
+      employee_age: "61",
+      profile_image: ""
+      },
+      {
+      id: "2",
+      employee_name: "Garrett Winters",
+      employee_salary: "170750",
+      employee_age: "63",
+      profile_image: ""
+      },
+      {
+      id: "3",
+      employee_name: "Ashton Cox",
+      employee_salary: "86000",
+      employee_age: "66",
+      profile_image: ""
+      },
+      {
+      id: "4",
+      employee_name: "Cedric Kelly",
+      employee_salary: "433060",
+      employee_age: "22",
+      profile_image: ""
+      },
+      {
+      id: "5",
+      employee_name: "Airi Satou",
+      employee_salary: "162700",
+      employee_age: "33",
+      profile_image: ""
+      },
+      {
+      id: "6",
+      employee_name: "Brielle Williamson",
+      employee_salary: "372000",
+      employee_age: "61",
+      profile_image: ""
+      },
+    ],
+  }
+
+  
+  render() {
+    return (
+    <div className ="container mt-5">
+      <div className ="row">
+        <div className ="col">
+          <form>
+            <div class="form-group">
+              <input type="email" class="form-control" id="exampleInputEmail1"  placeholder="Search by employee Name" aria-describedby="emailHelp"></input>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <table class="table">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Salary</th>
+                <th scope="col">Age</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
